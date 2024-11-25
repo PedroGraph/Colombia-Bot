9ffd0a0f0c65e77633f8b156ff5d962927b4f576
Pmedinam <p.medina@sell2rent.com>
2024-11-25 10:43:57 -0500

    Fixing command status

--- commands/playCommand.js
+++ commands/playCommand.js
@@ -5,11 +5,15 @@ export async function handlePlayCommand(message) {
     const command = message.content.toLowerCase();
 
     if (command === '!salir') return ;
+
+    if(message.startsWith('!') && !AUDIO_PATHS[command]){
+        message.reply('¡Comando no reconocido! Usa un comando válido como `!hptamonda`, `!fornicamos` o `!radiotiempo`.');
+        return false;
+    } 
     
     if (AUDIO_PATHS[command] && AUDIO_PATHS[command].audio) {
         await playAudio(message, AUDIO_PATHS[command].audio);
         return true;
-    } 
-    message.reply('¡Comando no reconocido! Usa un comando válido como `!hptamonda`, `!fornicamos` o `!radiotiempo`.');
+    }
     
 }
