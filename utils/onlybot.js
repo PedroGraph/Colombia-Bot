dd3ef0c0cd422c48d491862589cc7df80c0e5311
PedroGraph <pedrojosmed@gmail.com>
2025-10-19 11:40:31 -0500

    Deleting all javascript files

--- utils/onlybot.js
+++
@@ -1,6 +0,0 @@
-export function checkIfOnlyBot(voiceChannel) {
-    if (!voiceChannel) return false; 
-    const members = voiceChannel.members; 
-    const botMember = members.find((member) => member.user.bot); 
-    return members?.size < 2 && botMember?.user?.bot;
-}
